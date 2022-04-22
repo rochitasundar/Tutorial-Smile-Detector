45a0b3ef419e0284244fb8437110e3d0ec3d700f
Rochita Sundar <85839427+rochitasundar@users.noreply.github.com>
2022-04-21 18:42:54 -0700

    final commits

--- myapp2.py
+++ myapp2.py
@@ -1,26 +1,22 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import home, data, model, training
-
-
-try:
-    import keras.backend.tensorflow_backend as tb
-    tb._SYMBOLIC_SCOPE.value = True
-except ModuleNotFoundError:
-    pass
+from apps import home, data, training
 
 app = MultiApp()
 
+st.write(
+'''
+# Streamlit Tutorial
 
-st.write("""
-# Smile Detector Project
+'''
 
-""")
+)
 
-# https://icons.getbootstrap.com/
+
 app.add_app("Home", home.app, 'house-fill')
+
 app.add_app("Data", data.app, 'bar-chart-line-fill')
+
 app.add_app("Training", training.app, 'clock-fill')
-# app.add_app("Model", model.app, 'diagram-3-fill')
 
 app.run()
