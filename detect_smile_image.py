3748c8501047a5844a59dfdfd75da0cbe1ad0a51
Rochita Sundar <85839427+rochitasundar@users.noreply.github.com>
2022-06-23 07:56:33 -0700

    additional commits

--- detect_smile_image.py
+++ detect_smile_image.py
@@ -1,5 +1,6 @@
 # import the necessary packages
-from keras.preprocessing.image import img_to_array
+from tensorflow.keras.utils import img_to_array
+#from keras.preprocessing.image import img_to_array
 from keras.models import load_model
 import numpy as np
 import imutils
